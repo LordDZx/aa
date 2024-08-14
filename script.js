e7136d80d76cb8ae596ac6bcc13135c093c85070
LordDZx <sinperrr3ddd@gmail.com>
2024-08-14 16:51:04 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    window.location.href = 'http://taskthan.atwebpages.com/index.php';
+});
